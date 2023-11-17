74f1bbd64e93e5d00d08f9e26f1342cb52045fc0
Artit1 <quen92300@gmail.com>
2023-11-17 11:28:33 +0100

    Update Algorithm.py

--- AI/Algorithm.py
+++ AI/Algorithm.py
@@ -12,36 +12,33 @@ class character:
         self.position = position    #The position of the character on the board as a tuple (x,y)
         pass
 
-#We create a board class to set a template for all our generated boards
-class board:
-    def _init_(self,board_ID,char_list,instruction_list) -> None:
-        self.board_ID = board_ID                    #The unique ID of the board build on parents boards if any
-        self.char_list = char_list                  #The list of all charcaters on board
-        self.instruction_list = instruction_list    #The list of instructions required to reach the board
-        pass
 
+exemple = [['/' for j in range (7)] for i in range(7)]
 
-exemple = []
+exemple[0][0] = character("Arti Ficelle","0",True,391,35,3,(0,0))
+exemple[5][4] = character("Tofu 1","1",False,120,30,3,(5,4))
+exemple[4][5] = character("Tofu 2","2",False,110,32,3,(4,5))
 
-exemple.append(character("Arti Ficelle","0",True,391,35,3,(0,0)))
-exemple.append(character("Tofu 1","1",False,120,30,3,(5,4)))
-exemple.append(character("Tofu 2","2",False,110,32,3,(4,5)))
+exemple2 = [['/' for j in range (7)] for i in range(7)]
 
-exemple2 = []
-
-exemple2.append(character("Arti Ficelle","0",True,391,35,3,(4,4)))
-exemple2.append(character("Tofu 1","1",False,120,30,3,(5,4)))
-exemple2.append(character("Tofu 2","2",False,110,32,3,(4,5)))
+exemple2[0][1] = character("Arti Ficelle","0",True,391,35,3,(0,1))
+exemple2[6][5] = character("Tofu 1","1",False,120,30,3,(6,5))
+exemple2[4][6] = character("Tofu 2","2",False,110,32,3,(4,6))
 
 #For debug purpose
 def print_board(board):
-    tab = [['/' for i in range (7)] for j in range (7)]
-    for i in range(len(board)):
-        if(board[i].ally):
-            tab[board[i].position[0]][board[i].position[1]]="\033[92m" + board[i].ID + "\033[0m"
-        else:
-            tab[board[i].position[0]][board[i].position[1]]="\033[91m" + board[i].ID + "\033[0m"
-    return tab
+    for i in range(7):
+        for j in range(7):
+            try:
+                board[i][j].ally
+            except AttributeError:
+                print(board[i][j],end = "")
+            else:
+                if(board[i][j].ally):
+                    print("\033[92m" + board[i][j].ID + "\033[0m",end = "")
+                else:
+                    print("\033[91m" + board[i][j].ID + "\033[0m",end = "")
+        print("")
 
 #???
 def remplissage(character) :
@@ -51,71 +48,95 @@ def remplissage(character) :
             characters[i][j] = character[i*j]
 
 #This function test if a chosen cell is free to move on
-def IsCellFree(wanted_cell,char_list,ID):
+def IsCellFree(wanted_cell,board):
     available = True
-    for i in range (len(char_list)):
-        if(ID != char_list[i].ID):
-            if(wanted_cell == char_list[i].position):
-                available = False
+    if(board[wanted_cell[0]][wanted_cell[1]] != '/' and board[wanted_cell[0]][wanted_cell[1]].ID != "0" ):
+        available = False
     return available
 
 #A function to calcul all available deplacements
-def TheoricalMovment(starting_pos,char_list,mouvment,ID,n = 4):
+def TheoricalMovment(starting_pos,board,mouvment,n = 4):
     if (n != 0) :
         if(-1<starting_pos[0]<7 and -1<starting_pos[1]<7):
-            if(IsCellFree(starting_pos,exemple,ID)):
+            if(IsCellFree(starting_pos,board)):
                 mouvment[starting_pos[0]][starting_pos[1]] = 1
-                TheoricalMovment((starting_pos[0]+1,starting_pos[1]),char_list,mouvment,ID,n-1)
-                TheoricalMovment((starting_pos[0]-1,starting_pos[1]),char_list,mouvment,ID,n-1)
-                TheoricalMovment((starting_pos[0],starting_pos[1]+1),char_list,mouvment,ID,n-1)
-                TheoricalMovment((starting_pos[0],starting_pos[1]-1),char_list,mouvment,ID,n-1)
+                TheoricalMovment((starting_pos[0]+1,starting_pos[1]),board,mouvment,n-1)
+                TheoricalMovment((starting_pos[0]-1,starting_pos[1]),board,mouvment,n-1)
+                TheoricalMovment((starting_pos[0],starting_pos[1]+1),board,mouvment,n-1)
+                TheoricalMovment((starting_pos[0],starting_pos[1]-1),board,mouvment,n-1)
             else:
                 mouvment[starting_pos[0]][starting_pos[1]] = 2
 
 #A function to know how many foes are in the melee kayword
-def melee_count(position,char_list,ID):
-    cpt = 0
-    for i in range (len(char_list)):
-        if (ID != char_list[i].ID):
-            if (abs(char_list[i].position[0]-position[0])<=1) and (abs(char_list[i].position[1]-position[1])<=1):
-                cpt +=1
-    return cpt
+def melee_count(board):
+    for i in range(7):
+        for j in range(7):
+            try:
+                board[i][j].ally
+            except AttributeError:
+                pass
+            else:
+                if(board[i][j].ID=="0"):
+                    cpt = 0
+                    for k in range (3):
+                        for l in range (3):
+                            try:
+                                board[i+k-1][j+l-1].ally
+                            except AttributeError:
+                                pass
+                            else:
+                                if board[i+k-1][j+l-1].ally == False:
+                                    cpt+=1
+                    return cpt
+
 
 mouvment = [[0 for j in range (7)] for i in range(7)]    
 
-TheoricalMovment(exemple2[0].position,exemple2,mouvment,"0")
-for i in range (7):
-    for j in range (7):
-        print(mouvment[i][j],end="")
-    print("")
+TheoricalMovment((4,4),exemple,mouvment)
 
-tab = print_board(exemple2)
-for i in range (7):
-    for j in range (7):
-        print(tab[i][j],end="")
-    print("")
+for i in range(7):
+        for j in range(7):
+            print(mouvment[i][j],end = "")
+        print("")
 
-print(melee_count(exemple2[0].position,exemple2,"0"))
-
-<<<<<<< HEAD
 print(melee_count(exemple))
 
 
 def calcul(board,newboard) :
-    for i in range(board.len)
-    {
-        if (i = 1):
-        {
-            if(board[i].PV <= 0):
-            {
+    result = 0
+    for i in range(board.len):
+        if (i == 1):
+            if(newboard[i].PV <= 0):
                 result = result-10000
-            }
-        }
-        else
-        {
-
-        }
-    }
+            else:
+                result = result - (board[i].PV - newboard[i].PV)
+        else:
+            if(newboard[i].PV <= 0):
+                result = result + 2000 + newboard[i].atk
+            else:
+                result = result + 5*(board[i].PV - newboard[i].PV )
     return result
-=======
->>>>>>> 28efba16759175a73d6f17b425e15b239854e104
+
+
+def attack(board,perso):
+    mainpos = perso[0].position
+    for i in range(perso.len000):
+        if (i!=0):
+            TheoricalMovment(perso[i].position,board,mouvment,n=perso[i].PM)
+            if(mouvment[mainpos[0] + 1][mainpos[1]] == 1):
+                board[mainpos[0] + 1][mainpos[1]] = board[perso[i].position[0]][perso[i].position[1]]
+                board[perso[i].position[0]][perso[i].position[1]] = 0
+                perso[0].PV = perso[0].PV-perso[i].atk
+            elif(mouvment[mainpos[0]][mainpos[1]+1] == 1):
+                board[mainpos[0]][mainpos[1]+1] = board[perso[i].position[0]][perso[i].position[1]]
+                board[perso[i].position[0]][perso[i].position[1]] = 0
+                perso[0].PV = perso[0].PV-perso[i].atk
+            elif(mouvment[mainpos[0] - 1][mainpos[1]] == 1):
+                board[mainpos[0] - 1][mainpos[1]] = board[perso[i].position[0]][perso[i].position[1]]
+                board[perso[i].position[0]][perso[i].position[1]] = 0
+                perso[0].PV = perso[0].PV-perso[i].atk
+            elif(mouvment[mainpos[0] - 1][mainpos[1]] == 1):
+                board[mainpos[0] - 1][mainpos[1]] = board[perso[i].position[0]][perso[i].position[1]]
+                board[perso[i].position[0]][perso[i].position[1]] = 0
+                perso[0].PV = perso[0].PV-perso[i].atk
+    
