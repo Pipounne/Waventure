082f6934855156d5206efb548ca2f7904eb55ec7
Pipounne <fr.mister.r@gmail.com>
2023-12-04 09:39:13 +0100

    up the date

--- Data Recognition/Read_Data.py
+++ Data Recognition/Read_Data.py
@@ -2,32 +2,52 @@ from PIL import Image
 from PIL import ImageGrab
 from pytesseract import * 
 import numpy as np
+import pywinauto
+import time
 
-snapshot = ImageGrab.grab(bbox=(0,0,1343,881))
+basic_cells = [(155,155,50),(165,124,72),(154,143,62),(175,125,78),(182,149,113)]
+
 path_to_tesseract = r"C:\Users\antho\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
 
 pytesseract.tesseract_cmd = path_to_tesseract
 
-im_rgb = snapshot.convert('RGB')
+def samepixel(r,g,b):
+    for i in range (len(basic_cells)):
+        if((abs(basic_cells[i][0]-r) <= 10) and (abs(basic_cells[i][1]-g) <= 10) and (abs(basic_cells[i][2]-b) <= 10)):
+            return False
+    return True
 
 def reco_board():
-    for i in range (-6,7):
-        if(i%2==0):
-            for j in range (7-abs(i)):
-                snapshot.putpixel((370+(i+6)*50,480-int(50*(6-abs(i))/2)+50*j),(0,255,0))
-        else:   
-            for j in range (7-abs(i)):
-                snapshot.putpixel((370+(i+6)*50,480-int(50*(6-abs(i))/2)+50*j),(255,255,0))
+    image = ImageGrab.grab(bbox=(0,0,1343,882))
+    for i in range (7):
+          for j in range (7):
+            pixel = image.getpixel((375+i*50+j*50,475+i*25-j*25))
+            if(samepixel(pixel[0],pixel[1],pixel[2])):
+                pywinauto.mouse.click(button="left",coords=(375+i*50+j*50,475+i*25-j*25))
+                pywinauto.mouse.press(button="left",coords=(375+i*50+j*50,475+i*25-j*25))
+                time.sleep(0.7)
+                snapshot = ImageGrab.grab(bbox=(0,0,1343,80))
+                goodpixel=snapshot.getpixel((120,66))
+                badpixel=snapshot.getpixel((1210,66))
+                if(badpixel[0]==255 and badpixel[1]==255 and badpixel[2] == 255):
+                    print("mezzant")
+                elif(goodpixel[0]==255 and goodpixel[1]==255 and goodpixel[2] == 255):
+                    print("zentil")
+                pywinauto.mouse.release(button="left",coords=(375+i*50+j*50,475+i*25-j*25))
+                    
+            image.putpixel((375+i*50+j*50,475+i*25-j*25),(0,255,0))
 
     for i in range (7):
-        snapshot.putpixel((500+(i*63),820),(255,0,0))
+        print(image.getpixel((500+(i*63),820)))
+        image.putpixel((500+(i*63),820),(255,0,0))
 
-    snapshot.putpixel((941,820),(255,0,0))
+    image.putpixel((941,820),(255,0,0))
+    image.putpixel((1210,66),(0,255,0))
+    image.putpixel((120,66),(0,255,0))
+
+    image.show()
 
 reco_board()
-print(im_rgb)
-
-snapshot.show()
 
 #text = pytesseract.image_to_string(inverted_image)
 
